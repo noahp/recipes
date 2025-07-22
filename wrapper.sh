9f6fc28a5ffbce84615c001af82edd6a54145ed0
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2025-07-22 19:53:43 -0400

    remove trailing .html

--- wrapper.sh
+++ wrapper.sh
@@ -14,6 +14,13 @@ python3 generate-summary.py
 # if arg is "build", just build the book
 if [ "$1" = "build" ]; then
     mdbook build
+
+    # wipe off the .html extension
+    perl -i -p -e 's{href="index.html"}{href="."}g' book/toc.js
+    perl -i -p -e 's{(href="[a-zA-Z0-9.][^"]+)\.html"}{$1"}g' book/toc.js
+    find book/ -name *.html | xargs perl -i -p -e 's{href="index.html"}{href="."}g'
+    find book/ -name *.html | xargs perl -i -p -e 's{(href="[a-zA-Z0-9.][^"]+)\.html"}{$1"}g'
+
 else
     mdbook serve --hostname '0.0.0.0'
 fi
